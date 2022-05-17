c67fa1456b5b054b21a16f57f8913ca022a5dd5e
T.T <t.517a3@gmail.com>
2022-05-17 23:46:45 +0900

    Make a basic Shop model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Shop.create([
+  {name:"gundambase TOKYO"},
+  {name:"gundambase FUKUOKA"},
+  {name:"gundambase satellite KYOTO"}
+])
