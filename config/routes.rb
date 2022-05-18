fc59bbb5d666088a0a336a46863d45321c8aeb69
T.T <t.517a3@gmail.com>
2022-05-19 06:49:18 +0900

    Update controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'shops/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :shops
+  namespace :api do
+    namespace :v1 do
+      resources :shops
+    end
+  end
 end
