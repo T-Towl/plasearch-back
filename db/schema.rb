cdb57e69ceb24cfa4ad463a74c55bc608e088e9f
T.T <t.517a3@gmail.com>
2022-05-31 00:45:05 +0900

    Change shops column

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_17_140704) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_30_150340) do
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "lat"
+    t.decimal "lng"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Shop.create([
-  {name:"gundambase TOKYO"},
-  {name:"gundambase FUKUOKA"},
-  {name:"gundambase satellite KYOTO"}
+  {name: "gundambase TOKYO", lat: 35.62551386235291, lng: 139.77614366422262},
+  {name: "GUNDAM FACTORY YOKOHAMA", lat: 35.44670550526705, lng: 139.65406751562722},
+  {name: "ヨドバシカメラ AKIBA店", lat: 35.69912339804511, lng: 139.77463855336384}
 ])
