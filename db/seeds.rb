e035a43d7d7002b49ac0f90abe64923852d65325
Tsuji <86551586+PG-owl@users.noreply.github.com>
2022-07-13 10:35:39 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Shop.create([
-  {name: "gundambase TOKYO",lat: 35.62551386235291, lng: 139.77614366422262, address: "東京都江東区青海１丁目１−１０ ダイバーシティ東京 プラザ７F", opening_hours: 1100 , photo_reference: "https://lh5.googleusercontent.com/p/AF1QipO4LhYQfEgbvkkm0CDAutnfj_Hp2IUVYNySQPxi=w426-h240-k-no", rating: 4.5},
-  {name: "GUNDAM FACTORY YOKOHAMA", lat: 35.44670550526705, lng: 139.65406751562722, address: "神奈川県横浜市中区山下町 279番25 山下ふ頭内", opening_hours: 1100, photo_reference: "https://lh5.googleusercontent.com/p/AF1QipPhk77FYuR-SCULQOUO_cxPZq15C4uEcepOH_GR=w408-h272-k-no", rating: 4.4},
-  {name: "ヨドバシカメラ AKIBA店", lat: 35.69912339804511, lng: 139.77463855336384, address: "東京都千代田区神田花岡町１−１", opening_hours: 930, photo_reference: "https://lh5.googleusercontent.com/p/AF1QipPb_J4gv6mHIBDHZ0JNnMXGg2Sxe7Nv-VKZVFih=w408-h544-k-no", rating: 4.2}
+  {name: "gundambase TOKYO",lat: 35.62551386235291, lng: 139.77614366422262, address: "東京都江東区青海１丁目１−１０ ダイバーシティ東京 プラザ７F", opening_hours: 1100 , photo_reference: "https://lh5.googleusercontent.com/p/AF1QipO4LhYQfEgbvkkm0CDAutnfj_Hp2IUVYNySQPxi=w426-h240-k-no", rating: 4.5, place_id: "ChIJIy1S0_mJGGAR2d0UgvPKUPg"},
+  {name: "GUNDAM FACTORY YOKOHAMA", lat: 35.44670550526705, lng: 139.65406751562722, address: "神奈川県横浜市中区山下町 279番25 山下ふ頭内", opening_hours: 1100, photo_reference: "https://lh5.googleusercontent.com/p/AF1QipPhk77FYuR-SCULQOUO_cxPZq15C4uEcepOH_GR=w408-h272-k-no", rating: 4.4, place_id: "ChIJ92t8iPJdGGARfdfhGNI0Nsg"},
+  {name: "ヨドバシカメラ AKIBA店", lat: 35.69912339804511, lng: 139.77463855336384, address: "東京都千代田区神田花岡町１−１", opening_hours: 930, photo_reference: "https://lh5.googleusercontent.com/p/AF1QipPb_J4gv6mHIBDHZ0JNnMXGg2Sxe7Nv-VKZVFih=w408-h544-k-no", rating: 4.2, place_id: "ChIJGU_M2aeOGGARdnOLcMYXuXs"}
 ])
