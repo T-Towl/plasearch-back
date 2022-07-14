2dc0fc3983a764483b9a56a224fd685423d70d5f
T.T <t.517a3@gmail.com>
2022-07-14 22:43:43 +0900

    update migration_file

--- db/migrate/20220531135601_add_details_to_shops.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailsToShops < ActiveRecord::Migration[7.0]
-  def change
-    add_column :shops, :address, :string
-    add_column :shops, :opening_hours, :number
-    add_column :shops, :photo_reference, :string
-    add_column :shops, :rating, :number
-  end
-end
--- db/migrate/20220706135316_add_place_id_to_shops.rb
+++
@@ -1,5 +0,0 @@
-class AddPlaceIdToShops < ActiveRecord::Migration[7.0]
-  def change
-    add_column :shops, :place_id, :string
-  end
-end
