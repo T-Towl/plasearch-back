9712466379c5828b8cff0b8b1bb9dd9c9a81dbb5
T.T <t.517a3@gmail.com>
2022-07-20 07:23:56 +0900

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://clever-bubblegum-6b0de2.netlify.app"
 
     resource "*",
       headers: :any,
