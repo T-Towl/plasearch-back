a6790d72f33ee4321c7e3f36555c803417c02d56
T.T <t.517a3@gmail.com>
2022-07-21 20:47:15 +0900

    Initial setting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_30_150340) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_104826) do
   create_table "shops", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_150340) do
     t.decimal "rating"
     t.string "site_url"
     t.string "place_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
