0c13e0ad2fd94c0941efafac37264db8c8e5b92a
T.T <t.517a3@gmail.com>
2022-07-24 12:33:44 +0900

    Update users_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,30 @@
 class Api::V1::UsersController < ApplicationController
 
-  def new
+  def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 
+  def new
+    @user = User.new
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+    render json: @user
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+    end
 
 end
