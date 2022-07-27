7d82e5026e6e74589ee4882d3659e6762dde1732
T.T <t.517a3@gmail.com>
2022-07-27 23:48:13 +0900

    Add sessions_controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,26 +5,32 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+    # signup
   def new
-    @user = User.new
-    render json: @user
+    @user = User.new(user_params)
+
+    if @user.save
+        login!
+        render json: { status: :created, user: @user }
+    else
+        render json: { status: 500 }
+    end
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      # 保存の成功をここで扱う。
-    else
-      render 'new'
-    end
-    render json: @user
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     # 保存の成功をここで扱う。
+  #   else
+  #     render 'new'
+  #   end
+  #   render json: @user
+  # end
 
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
 end
