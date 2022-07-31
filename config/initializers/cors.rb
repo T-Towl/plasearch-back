c57b39bfdb2df84e941eae5046606410b4c81f36
T.T <t.517a3@gmail.com>
2022-07-31 12:13:30 +0900

    Change cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials.origin[:plasearch_front]
+    origins Rails.application.credentials.origin[:plasearch_front] , "http://localhost:3000/"
 
     resource "*",
       headers: :any,
