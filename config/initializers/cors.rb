b1432acd5a9ea2fe1a16cf2b033cc9f1a0f5a398
T.T <t.517a3@gmail.com>
2022-08-01 23:13:04 +0900

    cors 編集

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins Rails.application.credentials.origin[:plasearch_front] , 'http://localhost:3000/'
+    # origins Rails.application.credentials.origin[:plasearch_front]
+    origins 'http://localhost:3000/'
 
     resource "*",
       headers: :any,
