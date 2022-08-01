ab1a846305be6d18bb9db759667eee880d8c1aff
PG-owl <t.517a3@gmail.com>
2022-08-02 08:39:03 +0900

    sessions_controller 修正

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,10 +5,10 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(email: session_params[:email])
 
     if @user && @user.authenticate(session_params[:password])
-        login!
-        render json: { logged_in: true, user: @user }
+      login!
+      render json: { logged_in: true, user: @user }
     else
-        render json: { status: 401, errors: ['認証に失敗しました。', '正しいメールアドレス・パスワードを入力し直すか、新規登録を行ってください。'] }
+      render json: { status: 401, errors: ['認証に失敗しました。', '正しいメールアドレス・パスワードを入力し直すか、新規登録を行ってください。'] }
     end
   end
 
@@ -20,17 +20,17 @@ class Api::V1::SessionsController < ApplicationController
 
     # logged_in?
   def show
-      # logger.debug(@current_user)
-      if @current_user
-          render json: { logged_in: true, user: current_user }
-      else
-          render json: { logged_in: false, message: 'ユーザーが存在しません' }
-      end
+    # logger.debug(@current_user)
+    if @current_user
+      render json: { logged_in: true, user: current_user }
+    else
+      render json: { logged_in: false, message: 'ユーザーが存在しません' }
+    end
   end
 
   private
 
     def session_params
-        params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password)
     end
 end
