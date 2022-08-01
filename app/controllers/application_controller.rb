30ee4d665fe70a906e4df80dca87039f4b364a7b
PG-owl <t.517a3@gmail.com>
2022-08-01 13:59:15 +0900

    application_controller に cookiesとhelpersの設定を記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::API
 
   # skip_before_action :verify_authenticity_token
+  #Rails APIモードでCookiesを使用するには、以下を記述
   include ActionController::Cookies
+  #Rails APIモードでhelpersメソッドを使用するには、以下を記述
   include ActionController::Helpers
   helper_method :login!, :current_user
 
