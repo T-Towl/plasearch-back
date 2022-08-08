b067cf9897805123f3108e565e440a8c7c1f021c
T.T <t.517a3@gmail.com>
2022-08-08 21:34:09 +0900

    コンフリクト解消

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,12 +1,6 @@
 class Shop < ApplicationRecord
-<<<<<<< HEAD
 
   # React側でsearch機能を実装したため、一時コメントアウト
-
-=======
-  # React側でsearch機能を実装したため、一時コメントアウト
-  
->>>>>>> 12d98f26b237fa819caca265b54662b3f38e76c4
   # def self.search(search) #self.はShop.を意味する
   #   if search
   #     where(['username LIKE ?', "%#{search}%"]) #検索とuseanameの部分一致を表示。
