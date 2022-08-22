c7db0dc0f0a0ad7f9b3172d2c31fb4d533ff6d1a
T.T <t.517a3@gmail.com>
2022-08-22 20:22:31 +0900

    お気に入り機能 修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,8 @@ class Favorite < ApplicationRecord
   belongs_to :user   # ユーザー/お気に入り → 1:多
   belongs_to :shop   # 記事/お気に入り    → 1:多
 
-  validates :user_id, presence: true
-  validates_uniqueness_of :shop_id, scope: :user_id    # バリデーション（ユーザーと記事の組み合わせは一意）
-  # 同じ投稿を複数回お気に入り登録させないため。
+  # reference型を使うとデフォルトで必須バリデーションがかかる
+  # validates :user_id, presence: true
+  # validates_uniqueness_of :shop_id, scope: :user_id
+  # バリデーション（ユーザーと記事の組み合わせは一意）
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
+  get 'favorites/post'
+  get 'favorites/delete'
   get 'sessions/new'
   namespace :api do
     namespace :v1 do
       resources :shops, only: [:index, :show]
       resources :favorites, only: [:create, :destroy]
       resources :users do
-        get :favorites, on: :collection
+        get :favorites, on: :member
       end
       resources :sessions
     end
