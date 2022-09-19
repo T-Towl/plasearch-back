4bb365588618410228b45d2afdf4010c93df8503
T.T <t.517a3@gmail.com>
2022-09-19 10:49:46 +0900

    origin 修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,17 +6,8 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do 
-  #   origins Rails.application.credentials.origin[:plasearch_front]
-
-  #   resource "*",
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
-
   allow do 
-    origins "http://localhost:3000"
+    origins Rails.application.credentials.origin[:plasearch_front]
 
     resource "*",
       headers: :any,
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 if Rails.env === 'production'
-  # Rails.application.config.session_store :cookie_store, key: '_plasearch-back', domain: Rails.application.credentials.origin[:plasearch_front]
-  Rails.application.config.session_store :cookie_store, key: '_plasearch-back', domain: "http://localhost:3000"
+  Rails.application.config.session_store :cookie_store, key: '_plasearch-back', domain: Rails.application.credentials.origin[:plasearch_front]
 else
   Rails.application.config.session_store :cookie_store, key: '_plasearch-back'
 end
